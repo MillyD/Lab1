904a0a0d91bac7842c146136c4eaae975f20ea87
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-06 20:12:15 +0100

    Punkt.4 dodanie metody wypisującej tekst i zapoznanie z 3 typami komentarzy

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,7 +1,14 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
+    //
+    /*
+     */
+    /**
+     * The application's entry point
+     * @param args an array of command-line arguments for the application
+     */
     public static void main(String[] args) {
-
+        System.out.print("Hello Java world\n");
     }
 }

33ed693bcaf1d8ea82bd7cd2c998f798e2d0cf48
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-07 15:08:49 +0100

    Punkt.10.c) dodano padLeft do programu wypisującego liczbę w 3 systemach liczbowych

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -9,11 +9,18 @@ public class Main {
         do {
             System.out.println("Podaj liczbe: ");
             int num1 = scan.nextInt();
-            if( num1 == 0) break;
-            String bin = Integer.toBinaryString(num1);
-            String hex = Integer.toHexString(num1).toUpperCase();
-            System.out.printf("DEC = %d, BIN = %s, HEX = Ox%s\n",num1,bin,hex);
+            if (num1 == 0) break;
+            String bin = leftPad(Integer.toBinaryString(num1), '0', 4);
+            String hex = leftPad(Integer.toHexString(num1).toUpperCase(),'0',3);
+            System.out.printf("DEC = %d, BIN = %s, HEX = %%%s\n", num1, bin, hex);
         }
         while (true);
+    }
+    public static String leftPad(String aText, char aChar, int aWidth) {
+        String res = aText;
+        for (int i = 0; i < aWidth; i++) {
+            res = aChar + res;
+        }
+        return res;
     }
 }

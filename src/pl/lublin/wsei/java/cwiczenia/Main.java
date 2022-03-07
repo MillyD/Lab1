dda1b873dc49d43cbaf4babaa05cb8e68df17ffb
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-07 21:13:52 +0100

    Punkt.12 Transliteracja ukraińskich imion i nazwisk

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -4,8 +4,20 @@ import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
 public class Main {
     public static void main(String[] args) {
-        Account acc = new Account();
-        acc.setName("piotr gołabeK");
-        System.out.println(acc.getName());
+        Account acc1 = new Account();
+        acc1.setName("ВЕЛИЧКО Глуховский"); //VELYCHKO glukhovsky
+        Account acc2 = new Account();
+        acc2.setName("Наталія Кличко"); //Nataliya Klychko
+        Account acc3 = new Account();
+        acc3.setName("Олена Леоненко"); //Olena Leonenko
+        Account acc4 = new Account();
+        acc4.setName("JARosław Węgierski");
+
+        System.out.println(acc1.getName());
+        System.out.println(acc2.getName());
+        System.out.println(acc3.getName());
+        System.out.println(acc4.getName());
+
+
     }
 }
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -5,15 +5,37 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Account {
     public String name;
+    final private static String ukrAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+    final private static String[] translitRepl = {"a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j",
+            "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c", "ch", "sh", "shh", "'", "ju", "ja"};
 
     public void setName(String name) {
-        this.name = capitalize(name);
+        this.name = capitalize(translit(name));
     }
-    public static String capitalize(String name) {
+
+    private static String capitalize(String name) {
         String[] tokens = StringUtils.split(name);
-        for(int i = 0; i < tokens.length; i++)
+        for (int i = 0; i < tokens.length; i++)
             tokens[i] = StringUtils.capitalize(tokens[i].toLowerCase());
-        return StringUtils.join(tokens,' ');
+        return StringUtils.join(tokens, ' ');
+    }
+
+    private static String translit(String arg) {
+        String argLowerCase = arg.toLowerCase();
+        String[] part = StringUtils.split(argLowerCase);
+        String[] result = {"",""};
+        int indx;
+        for(int i = 0 ; i < part.length; i++) {
+            for (int j = 0; j < part[i].length(); j++) {
+                indx = StringUtils.indexOf(ukrAlphabet, part[i].charAt(j));
+                if (indx == -1) {
+                    result[i] += (part[i].charAt(j));
+                } else {
+                    result[i] += (translitRepl[indx]);
+                }
+            }
+        }
+        return StringUtils.join(result,' ');
     }
 
     public String getName() {

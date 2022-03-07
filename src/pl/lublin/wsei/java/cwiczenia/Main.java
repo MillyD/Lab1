24aaf172873f47e22d9e2fb50cf91daf903f86d1
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-07 17:20:37 +0100

    Punkt.12 utworzono nową gałąź mylib pakietu pl.lublin.wsei.java

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,26 +1,9 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        do {
-            System.out.println("Podaj liczbe: ");
-            int num1 = scan.nextInt();
-            if (num1 == 0) break;
-            String bin = leftPad(Integer.toBinaryString(num1), '0', 4);
-            String hex = leftPad(Integer.toHexString(num1).toUpperCase(),'0',3);
-            System.out.printf("DEC = %d, BIN = %s, HEX = %%%s\n", num1, bin, hex);
-        }
-        while (true);
-    }
-    public static String leftPad(String aText, char aChar, int aWidth) {
-        String res = aText;
-        for (int i = 0; i < aWidth; i++) {
-            res = aChar + res;
-        }
-        return res;
+
+
     }
 }

20d13a344aa4698f6ab7f2900b22ffe8f7fef291
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-07 18:28:12 +0100

    Punkt.12 wydzielenie działań korygujących pisownie do oddzielnej metody statycznej

--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -2,17 +2,22 @@ package pl.lublin.wsei.java.cwiczenia.mylib;
 
 import org.apache.commons.lang3.StringUtils;
 
+
 public class Account {
-    private String name;
+    public String name;
 
     public void setName(String name) {
+        this.name = capitalize(name);
+    }
+    public static String capitalize(String name) {
         String[] tokens = StringUtils.split(name);
-        for (int i = 0; i < tokens.length; i++)
+        for(int i = 0; i < tokens.length; i++)
             tokens[i] = StringUtils.capitalize(tokens[i].toLowerCase());
-        this.name = StringUtils.join(tokens, ' ');
+        return StringUtils.join(tokens,' ');
     }
 
     public String getName() {
         return name;
     }
+
 }

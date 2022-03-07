79394f552219f4d85455b9245c0cb8ca74fc6db9
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-07 11:08:55 +0100

    Punkt.7 zapoznanie z metodami System.out, wypisującymi tekst na ekran

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,14 +1,24 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
-    //
-    /*
-     */
-    /**
-     * The application's entry point
-     * @param args an array of command-line arguments for the application
-     */
+
     public static void main(String[] args) {
-        System.out.print("Hello Java world\n");
+
+        System.out.println("Metoda print: ");
+        System.out.print("Ala");
+        System.out.print("ma");
+        System.out.print("kota");
+        System.out.print("\n");
+
+        System.out.println("Metoda println:");
+        System.out.println("Ola");
+        System.out.println("ma");
+        System.out.println("psa");
+        System.out.println("Metoda printf:");
+
+        String smt = "Rower";
+        double pr = 1234.564;
+        int wh = 4;
+        System.out.printf("Kasia ma %20s o %d kołach za %.2f zł ",smt,wh,pr);
     }
 }

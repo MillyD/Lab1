e7a9972d6fa17bd0924f1c97717f76aa6a5ffd57
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-07 13:03:30 +0100

    Punkt.9.d) komenda break. wykorzystanie w pętli

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,18 +1,21 @@
 package pl.lublin.wsei.java.cwiczenia;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num1,num2;
-       do {
-           System.out.println("Podaj pierwszą liczbę: ");
-           num1 = scan.nextInt();
-           System.out.println("Podaj drugą liczbę: ");
-           num2 = scan.nextInt();
-           System.out.printf("Wynik dodawania: %d + %d = %d%n", num1, num2, num1 + num2);
-       }
-       while((num1 != 0) && (num2 != 0));
+        int num1, num2;
+        do {
+            System.out.println("Podaj pierwszą liczbę: ");
+            num1 = scan.nextInt();
+            if(num1 == 0) break;
+            System.out.println("Podaj drugą liczbę: ");
+            num2 = scan.nextInt();
+            if(num2 == 0) break;
+            System.out.printf("Wynik dodawania: %d + %d = %d%n", num1, num2, num1 + num2);
+        }
+        while (true);
     }
 }
